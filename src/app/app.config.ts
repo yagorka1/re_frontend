9fbeac0d578d51dea6b6525742eb590cc4f20cdb
Egor Grinevich <yahor_hrynevich@velmie.com>
2026-09-04 13:34:37 +0300

    ci: setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,7 @@ import { provideClientHydration } from '@angular/platform-browser';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration()
-  ]
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
